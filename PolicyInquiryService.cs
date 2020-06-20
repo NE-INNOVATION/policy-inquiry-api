9563d1b1dd57d0b057e8440d14a5f5a329a1b38c
Ganesh <ganesh.bhogle88@gmail.com>
2020-06-20 11:43:18 -0400

    code chnages for logging

--- PolicyInquiryService.cs
+++ PolicyInquiryService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace policy_Inquiry_api
@@ -9,13 +10,19 @@ namespace policy_Inquiry_api
 	public class PolicyInquiryService
 	{
         private readonly IMongoCollection<PolicyInquiry> _policyInquiry;
+        private readonly ILogger<PolicyInquiryService> _logger;
 
-        public PolicyInquiryService(IIrquiDatabaseSettings settings)
+        public PolicyInquiryService(IIrquiDatabaseSettings settings, ILogger<PolicyInquiryService> logger)
         {
             var client = new MongoClient(System.Environment.GetEnvironmentVariable("ConnectionString"));
             var database = client.GetDatabase(System.Environment.GetEnvironmentVariable("DatabaseName"));
 
             _policyInquiry = database.GetCollection<PolicyInquiry>(System.Environment.GetEnvironmentVariable("PolicyInquiryCollectionName"));
+
+            _logger = logger;
+            _logger.LogInformation("ConnectionString" + System.Environment.GetEnvironmentVariable("ConnectionString"));
+            _logger.LogInformation("ConnectionString" + System.Environment.GetEnvironmentVariable("DatabaseName"));
+            _logger.LogInformation("ConnectionString" + System.Environment.GetEnvironmentVariable("PolicyInquiryCollectionName"));
         }
 
         public List<PolicyInquiry> Get() =>
